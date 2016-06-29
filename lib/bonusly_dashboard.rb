000d9d672eb08412cdced04eb4407bc7e058f6d3
Robert Ingrum <robert@Roberts-MacBook-Pro.local>
2016-06-29 15:11:10 -0600

    Final update to get all of the files into the asset pipeline.

--- lib/bonusly_dashboard.rb
+++ lib/bonusly_dashboard.rb
@@ -3,7 +3,7 @@ require 'rails'
 module BonuslyDashboard
   class Engine < Rails::Engine
     initializer :assets do |config|
-      Rails.application.config.assets.precompile += %w{ app.js index.css }
+      Rails.application.config.assets.precompile += %w{ app.js Bonus.js BonuslyDashboard.js BonusManager.js jquery-1.12.4.min.js Manager.js Receiver.js StatManager.js Util.js index.css }
       Rails.application.config.assets.paths << root.join('app', 'assets', 'javascripts')
       Rails.application.config.assets.paths << root.join('app', 'assets', 'stylesheets')
     end
