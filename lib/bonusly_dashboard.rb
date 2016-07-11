e1762e41a85043540701bc014a5337e45d91e1dd
Robert Ingrum <robert@Roberts-MacBook-Pro.local>
2016-07-11 12:22:58 -0600

    Increasing version number and registering the spinner as an asset

--- lib/bonusly_dashboard.rb
+++ lib/bonusly_dashboard.rb
@@ -3,9 +3,10 @@ require 'rails'
 module BonuslyDashboard
   class Engine < Rails::Engine
     initializer :assets do |config|
-      Rails.application.config.assets.precompile += %w{ app.js Bonus.js BonuslyDashboard.js BonusManager.js jquery-1.12.4.min.js Manager.js Receiver.js StatManager.js Util.js index.css }
+      Rails.application.config.assets.precompile += %w{ app.js Bonus.js BonuslyDashboard.js BonusManager.js jquery-1.12.4.min.js Manager.js Receiver.js StatManager.js Util.js index.css spinner_64.gif }
       Rails.application.config.assets.paths << root.join('app', 'assets', 'javascripts')
       Rails.application.config.assets.paths << root.join('app', 'assets', 'stylesheets')
+      Rails.application.config.assets.paths << root.join('app', 'assets', 'images')
     end
   end
 end
