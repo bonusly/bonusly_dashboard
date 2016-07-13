9a35db6028a0d399cf3dd3ca877a96ceac2f81f5
Robert Ingrum <rkingrum@gmail.com>
2016-07-13 17:03:24 -0600

    Allowing iframes and making things slightly more responsive.

--- app/controllers/bonusly_dashboard/dashboard_controller.rb
+++ app/controllers/bonusly_dashboard/dashboard_controller.rb
@@ -1,6 +1,7 @@
 module BonuslyDashboard
   class DashboardController < ApplicationController
     skip_after_filter :intercom_rails_auto_include
+    after_filter :allow_iframe, only: :index
 
     def index
       render layout: false
@@ -9,6 +10,11 @@ module BonuslyDashboard
     def version
       response = {status: :ok, message: Gem.loaded_specs['bonusly_dashboard'].version.to_s}
       render json: response
+    end
+
+    private
+    def allow_iframe
+      response.headers.except! 'X-Frame-Options'
     end
   end
 end
