d2026d292a0bfd57b6a0be099cbdf4a4a6fffabc
Robert Ingrum <rkingrum@gmail.com>
2016-07-18 14:57:22 -0600

    Trying to get assets served correctly

--- lib/bonusly_dashboard.rb
+++ lib/bonusly_dashboard.rb
@@ -2,13 +2,6 @@ require 'rails'
 require 'font-awesome-sass'
 
 module BonuslyDashboard
-  class Engine < Rails::Engine
-    initializer :assets do |config|
-      Rails.application.config.assets.precompile += [
-          'app.js', 'Bonus.js', 'BonuslyDashboard.js', 'BonusManager.js', 'jquery-1.12.4.min.js', 'Manager.js',
-          'Receiver.js', 'StatManager.js', 'Util.js', 'index.sass' ]
-      Rails.application.config.assets.paths << root.join('app', 'assets', 'javascripts')
-      Rails.application.config.assets.paths << root.join('app', 'assets', 'stylesheets')
-    end
+  class Engine < ::Rails::Engine
   end
 end
