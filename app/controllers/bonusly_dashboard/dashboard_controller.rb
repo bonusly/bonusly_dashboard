709ce989c8050487c1fcbe0009492a6ee3ce257c
Jon Evans <joncodes+github@gmail.com>
2016-11-29 11:50:06 -0700

    Prepare for encrypted keys (#5)
    * Prefer access_token from param over current user's token
    
    * Move data fetching into controller
    
    * Bump version
    
    * Improve validation of provided access token

--- app/controllers/bonusly_dashboard/dashboard_controller.rb
+++ app/controllers/bonusly_dashboard/dashboard_controller.rb
@@ -2,9 +2,15 @@ module BonuslyDashboard
   class DashboardController < ApplicationController
     skip_after_filter :intercom_rails_auto_include
     after_filter :allow_iframe, only: :index
+    before_filter :ensure_api_key, only: :index
 
     def index
       override_x_frame_options('ALLOW-FROM *')
+
+      @access_token = access_token
+      @company      = company
+      @theme_color  = theme_color
+
       render layout: false
     end
 
@@ -14,6 +20,33 @@ module BonuslyDashboard
     end
 
     private
+
+    def access_token
+      @access_token ||= params[:access_token].presence || current_user&.api_key&.access_token
+    end
+
+    def company
+      @company ||= begin
+        if api_key&.api_authenticatable.is_a?(Company)
+          api_key.api_authenticatable
+        elsif api_key&.api_authenticatable.is_a?(User) && api_key.api_authenticatable.active?
+          api_key.api_authenticatable.company
+        end
+      end
+    end
+
+    def theme_color
+      company.nil? ? BONUSLY::THEMES['dark']['brand'] : company_theme(company)[:brand]
+    end
+
+    def api_key
+      @api_key ||= ApiKey.for_access_token(access_token).first
+    end
+
+    def ensure_api_key
+      render text: 'Invalid access token provided', status: 401 unless api_key.present?
+    end
+
     def allow_iframe
       response.headers.except! 'X-Frame-Options'
     end
