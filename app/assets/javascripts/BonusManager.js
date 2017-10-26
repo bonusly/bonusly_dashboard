2ef4934d8c591b27bb20459a4170cf88271a8b86
Robert Ingrum <rkingrum@gmail.com>
2017-10-26 13:57:25 -0600

    Fixings

--- app/assets/javascripts/BonusManager.js
+++ app/assets/javascripts/BonusManager.js
@@ -1,35 +1,39 @@
 function BonusManager(parent) {
-  this.parent = parent;
-
-  this.bonuses = [];
+  this.parent       = parent;
+  this.bonuses      = [];
   this.currentBonus = 0;
 }
 
 BonusManager.prototype = {
-
   build: function() {
     this.bonuses      = [];
     this.currentBonus = 0;
     var bonus_data    = this.parent.callback_response.bonuses;
 
-    this.bonuses = this.bonuses.concat( $.grep(bonus_data.result, function(bonus, _) { return Util.useBonus(bonus) }) )
-        .map( function(item) { return new Bonus(item) } );
+    this.bonuses = this.bonuses.concat(
+        $.grep(
+            bonus_data.result,
+            function(bonus, _) { return Util.useBonus(bonus) }
+        )
+    ).map( function(item) { return new Bonus(item) } );
 
     this.bonuses = Util.shuffle(this.bonuses);
   },
 
   showOnInterval: function() {
     var noBonusMessage = $('.no-bonus-message');
-    if (this.bonuses == undefined || this.bonuses.length == 0) {
+
+    if (this.bonuses.length === 0) {
       noBonusMessage.show();
       return;
     } else {
       noBonusMessage.hide();
     }
 
-    var bonus = this.bonuses[this.currentBonus];
+    var bonus         = this.bonuses[this.currentBonus];
     this.currentBonus = (this.currentBonus + 1) % this.bonuses.length;
-    if (bonus != undefined) bonus.show();
+
+    if (bonus !== undefined) bonus.show();
   },
   
   showOnLoad: function() {}
--- app/assets/javascripts/StatManager.js
+++ app/assets/javascripts/StatManager.js
@@ -1,12 +1,10 @@
 function StatManager(parent) {
-  this.parent = parent;
-
-  this.stats = [];
+  this.parent        = parent;
+  this.stats         = [];
   this.curStatNumber = 0;
 }
 
 StatManager.prototype = {
-
   build: function() {
     this.stats     = [];
     var stat_data  = this.parent.callback_response.stats;
@@ -15,27 +13,36 @@ StatManager.prototype = {
     bonus_data.result.stat_type = 'recent_receivers';
     this.stats = this.stats.concat( new Stat(bonus_data.result) );
 
-    this.stats = this.stats.concat($.map(stat_data.result, function(item) { return new Stat(item)})
-        .filter( function(item) { return item != null; } ));
+    this.stats = this.stats.concat(
+        $.map(
+            stat_data.result,
+            function(item) { return new Stat(item) }
+        ).filter(
+            function(item) { return item != null }
+        )
+    );
   },
 
   showOnInterval: function() {},
   
   showOnLoad: function() {
-    var count = 0;
+    if (this.stats.length === 0) { return }
+
+    var count            = 0;
     var $highlightedStat = $('.highlighted-stat');
 
-    $highlightedStat.fadeOut(Util.seconds(1), function() {
-      $highlightedStat.remove();
-    });
-
-    if (this.stats.length == 0) { return; }
+    $highlightedStat.fadeOut(
+        Util.seconds(1),
+        function() { $highlightedStat.remove() }
+    );
 
     while (count < 2) {
       count++;
       this.stats[this.curStatNumber].show();
       this.curStatNumber++;
-      if (this.curStatNumber == this.stats.length) this.curStatNumber = 0;
+      if (this.curStatNumber === this.stats.length) {
+        this.curStatNumber = 0;
+      }
     }
 
     $highlightedStat.fadeIn(Util.seconds(2));
