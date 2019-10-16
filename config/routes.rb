aa523667b4444487382c657822540fd6f45c863c
Bryan Thompson <bryan.thompson@bonus.ly>
2019-10-16 12:41:12 -0600

    Redirect old dashboard routes to new digital signage routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'company/dashboard' => 'bonusly_dashboard/dashboard#index'
-  get 'company/dashboard/version' => 'bonusly_dashboard/dashboard#version'
-  get 'company/dashboard/data' => 'bonusly_dashboard/dashboard#data'
+  get 'company/dashboard', to: redirect('company/digital_signage')
+  get 'company/dashboard/version', to: redirect('company/digital_signage/version')
+  get 'company/dashboard/data', to: redirect('company/digital_signage/data')
   get 'company/digital_signage' => 'bonusly_dashboard/dashboard#index'
   get 'company/digital_signage/version' => 'bonusly_dashboard/dashboard#version'
   get 'company/digital_signage/data' => 'bonusly_dashboard/dashboard#data'
