68d8561d8a3933e3f6beaf68cd703badb1c611aa
Bryan Thompson <bryan.thompson@bonus.ly>
2019-10-16 09:20:26 -0600

    Added digital signage routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get 'company/dashboard' => 'bonusly_dashboard/dashboard#index'
   get 'company/dashboard/version' => 'bonusly_dashboard/dashboard#version'
   get 'company/dashboard/data' => 'bonusly_dashboard/dashboard#data'
+  get 'company/digital_signage' => 'bonusly_dashboard/dashboard#index'
+  get 'company/digital_signage/version' => 'bonusly_dashboard/dashboard#version'
+  get 'company/digital_signage/data' => 'bonusly_dashboard/dashboard#data'
 end
