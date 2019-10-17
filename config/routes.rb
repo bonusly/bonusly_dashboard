deabb5b24c3e382780dbf7ee7d0e0a9a0f6e3ac8
Bryan Thompson <bryan.thompson@bonus.ly>
2019-10-17 08:07:17 -0600

    Added path to data redirect as access token is needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'company/dashboard', to: redirect(path: 'company/digital_signage')
   get 'company/dashboard/version', to: redirect('company/digital_signage/version')
-  get 'company/dashboard/data', to: redirect('company/digital_signage/data')
+  get 'company/dashboard/data', to: redirect(path: 'company/digital_signage/data')
   get 'company/digital_signage' => 'bonusly_dashboard/dashboard#index'
   get 'company/digital_signage/version' => 'bonusly_dashboard/dashboard#version'
   get 'company/digital_signage/data' => 'bonusly_dashboard/dashboard#data'
