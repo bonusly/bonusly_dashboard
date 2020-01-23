badcaa4e43a39b13ab30c090608cb4f32cdcbcdb
Jon Evans <joncodes+github@gmail.com>
2020-01-23 09:29:17 -0700

    Don't even try to get access_token from URL

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,12 +1,8 @@
 $(document).ready(function() {
   var urlParams = Util.getUrlParams();
-  var accessToken = urlParams['access_token'];
+  var accessToken = $('.highlights').data('access-token');
 
-  if (accessToken == '' || accessToken == undefined) {
-    accessToken = $('.highlights').data('access-token');
-  }
-
-  var options =   $.extend({accessToken: accessToken}, urlParams);
+  var options =   $.extend({ accessToken: accessToken }, urlParams);
 
   new BonuslyDashboard(options);
 });
